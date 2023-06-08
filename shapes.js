e9fbe2f46d64853ae57207e9133d660892ad3cff
ColtenZimm <coltenz00@gmail.com>
2023-06-08 18:34:51 -0500

    added shapes

--- shapes.js
+++ shapes.js
@@ -1,34 +1,31 @@
-//npm package to valid for CSS3 compatible color value.
-const isCss3Color = require('is-css3-color');
-class Shape {
-  constructor({ logoName, textColor, logoColor, logoShape }) {
-    this.logoShape = logoShape;
-    this.validTextInput(logoName);
-    this.logoName = logoName;
-    this.validColorInput(textColor);
-    this.textColor = textColor;
-    this.validColorInput(logoColor);
-    this.logoColor = logoColor;
+class Shape{
+  // Defines a class called Shape which has a constructor intializing 'color' and sets the 'color' value.
+  
+      constructor(){
+          this.color=''
+      }
+      setColor(color){
+          this.color=(color);
+      }
   }
-  ifInputEmpty(input) {
-    if (!input) throw new Error('Input cannot be empty');
+  // Defines a Circle class that extends Shape and renders an SVG Circle with position, size, and fill color based on the current instance's properties.
+  class Circle extends Shape{
+      render(){
+          return `<circle cx="50%" cy="50%" r="100" height="100%" width="100%" fill="${this.color}">`
+      }
   }
-  validTextInput(input) {
-    this.ifInputEmpty(input);
-    if (input.length > 3) {
-      throw new Error('Logo text cannot be more than 3 characters');
-    }
+  // Defines a Square class that extends Shape and renders an SVG Square with position, size, and fill color based on the current instance's properties.
+  class Square extends Shape{
+      render(){
+          return `<rect x="50" height="200" width="200" fill="${this.color}">`
+      }
   }
-  validColorInput(input) {
-    this.ifInputEmpty(input);
-    //Change all css named color to lowercase
-    input = input.toLowerCase();
-    if (!isCss3Color(input)) {
-      throw new Error('Please enter a valid css color keyword or hex code');
-    }
-  }
-  render() {
-    throw new Error('Child shapes must implement a render() method');
-  }
-}
-module.exports = Shape;
+  // Defines a Triangle(Polygon) class that extends Shape and renders an SVG Triangle(Polygon) with position, size, and fill color based on the current instance's properties.
+  class Triangle extends Shape{
+      render(){
+          //return `<polygon height="100%" width="100%" points="25,75 75,25 25,25" fill="${this.color}">'
+          return `<polygon height="100%" width="100%" points="0,200 300,200 150,0" fill="${this.color}">`
+      }
+  };
+  
+  module.exports = {Circle, Square, Triangle}
